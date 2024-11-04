7656e1f0b4e2a94262cf868f390fc900a14e294b
franciscosousafd <francisco.sousa@fanduel.com>
2024-11-04 14:45:13 +0000

    chore: update the code to be easier to change to any sport or team

--- cypress/e2e/0-Sanity/fanduel.cy.js
+++ cypress/e2e/0-Sanity/fanduel.cy.js
@@ -6,59 +6,62 @@ context('Actions', () => {
      * This is for the hackaton, so the code it's hardcoed
      */
     it('Click on fanduel page', () => {
-        cy.viewport(1920, 1080);
-        const timetoutValue = 5000;
-
-        const baseUrl = "https://co.sportsbook.fanduel.com/";
-        cy.visit(baseUrl); // Only works with prod
-
-        cy.wait(timetoutValue);
-        cy.get('div ul li span').contains('NHL').click();
-
-
-        cy.wait(timetoutValue);
-
-        cy.viewport(1920, 1080);
-
-        //Get Seattle
-
-        cy.get('span[aria-label="Seattle Kraken"]').click();
-        cy.wait(timetoutValue);
+      const timeOutValue = 5000;
+      const baseUrl = "https://co.sportsbook.fanduel.com/";
+      const sportName = "NHL";
+      const fullTeamName = "Los Angeles Kings";
+      const teamName = "Los Angeles";
+      const loginEmail = "hackaton2024@fanduel.com";
+      const loginPassword = "potatoes";
 
 
-        //Click on Seattle and will be selected
-        cy.get('div[role="button"][aria-label*="Seattle"]').first().click();
-        cy.wait(timetoutValue);
+      cy.viewport(1920, 1080); //define that we want the test to execute in full hd resolution
+
+      cy.visit(baseUrl); // Only works with prod
+      cy.wait(timeOutValue);
+
+      //Click on the sport that we want to bet
+      cy.get('div ul li span').contains(sportName).click();
+      cy.wait(timeOutValue);
+
+      
+      //Click on the team that we want to bet
+      cy.get(`span[aria-label="${fullTeamName}"]`).first().click();
+      cy.wait(timeOutValue);
 
 
-        // click on wager button
-        cy.get('div').find('input[type="text"]').first().type('0.09');
-        cy.wait(timetoutValue);
-
-        //Click again on Seattle and will be de-selected
-
-        cy.get('div[role="button"][aria-label*="Seattle"]').first().click();
-        cy.wait(timetoutValue);
+      //Click on the team that we want selected
+      betOnFirstTeam(teamName);
+      cy.wait(timeOutValue);
 
 
-        cy.get('div[role="button"][aria-label*="Seattle"]').first().click();
-        cy.get('div').find('input[type="text"]').first().type('0.10');
+      // click on wager button
+      cy.get('div').find('input[type="text"]').first().type('0.09');
+      cy.wait(timeOutValue);
 
-        cy.wait(timetoutValue);
+      //Click again on team and will be de-selected
 
-        //login to do the bet
+      betOnFirstTeam(teamName);
+      cy.wait(timeOutValue);
 
-        /*const loginEmail = "francisco.sousa+usft.01@fanduel.com";
-        const loginPassword = "batatas";
+      //Click again to bet again in the team
+      betOnFirstTeam(teamName);
+      cy.get('div').find('input[type="text"]').first().type('0.10');
+      cy.wait(timeOutValue);
 
+      //login to do the bet
+      cy.wait(timeOutValue);
+      cy.get('#root div > div ').contains('Log in').click();
+      cy.get('#login-email').type(loginEmail);
+      cy.get('#login-password').type(loginPassword);
+      cy.wait(timeOutValue);
+      //cy.get('main div section div form button[type="submit"]').click();
 
-        cy.get('#root div > div ').contains('Log in').click();
-        cy.get('#login-email').type(loginEmail);
-        cy.get('#login-password').type(loginPassword);
-        cy.wait(timetoutValue);
-        //cy.get('main div section div form button[type="submit"]').click();
-
-        */
+      cy.wait(timeOutValue);
       })
 
 })
+
+function betOnFirstTeam(teamName) {
+  cy.get(`div[role="button"][aria-label*="${teamName}"]`).first().click();
+}
