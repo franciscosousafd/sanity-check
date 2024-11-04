1884d6723edcc8d376f5ef366d5c287a9fc3b47f
franciscosousafd <francisco.sousa@fanduel.com>
2024-11-04 15:23:49 +0000

    chore: Remove unnecessary logs

--- cypress/e2e/0-Sanity/fanduel.cy.js
+++ cypress/e2e/0-Sanity/fanduel.cy.js
@@ -6,11 +6,13 @@ context('Actions', () => {
      * This is for the hackaton, so the code it's hardcoed
      */
     it('Click on fanduel page', () => {
-      const timeOutValue = 5000;
+      cy.intercept('**', { log: false });
+      
+      
+      const timeoutValue = 5000;
       const baseUrl = "https://co.sportsbook.fanduel.com/";
       const sportName = "NHL";
       const fullTeamName = "Los Angeles Kings";
-      const teamName = "Los Angeles";
       const loginEmail = "hackaton2024@fanduel.com";
       const loginPassword = "potatoes";
 
@@ -18,46 +20,46 @@ context('Actions', () => {
       cy.viewport(1920, 1080); //define that we want the test to execute in full hd resolution
 
       cy.visit(baseUrl); // Only works with prod
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
 
       //Click on the sport that we want to bet
       cy.get('div ul li span').contains(sportName).click();
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
 
       
       //Click on the team that we want to bet
       cy.get(`span[aria-label="${fullTeamName}"]`).first().click();
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
 
 
       //Click on the team that we want selected
-      betOnFirstTeam(teamName);
-      cy.wait(timeOutValue);
+      betOnFirstTeam(fullTeamName);
+      cy.wait(timeoutValue);
 
 
       // click on wager button
       cy.get('div').find('input[type="text"]').first().type('0.09');
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
 
       //Click again on team and will be de-selected
 
-      betOnFirstTeam(teamName);
-      cy.wait(timeOutValue);
+      betOnFirstTeam(fullTeamName);
+      cy.wait(timeoutValue);
 
       //Click again to bet again in the team
-      betOnFirstTeam(teamName);
+      betOnFirstTeam(fullTeamName);
       cy.get('div').find('input[type="text"]').first().type('0.10');
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
 
       //login to do the bet
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
       cy.get('#root div > div ').contains('Log in').click();
       cy.get('#login-email').type(loginEmail);
       cy.get('#login-password').type(loginPassword);
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
       //cy.get('main div section div form button[type="submit"]').click();
 
-      cy.wait(timeOutValue);
+      cy.wait(timeoutValue);
       })
 
 })
