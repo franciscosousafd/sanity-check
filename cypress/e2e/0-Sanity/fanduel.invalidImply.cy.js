74b7640a5586f44345ce4ddeb731a7102ce9d5dc
franciscosousafd <francisco.sousa@fanduel.com>
2024-11-05 10:32:56 +0000

    chore: Add test for an invalid Imply bet

---
+++ cypress/e2e/0-Sanity/fanduel.invalidImply.cy.js
@@ -0,0 +1,56 @@
+/// <reference types="cypress" />
+
+
+context('Actions', () => {
+    /**
+     * This is for the hackaton, so the code it's hardcoed
+     */
+    it('Click on fanduel page', () => {
+      cy.intercept('**', { log: false });
+      
+      const timeoutValue = 5000;
+      const baseUrl = "https://co.sportsbook.fanduel.com/";
+      const sportName = "NHL";
+      const fullTeamName = "Los Angeles Kings";
+      
+      cy.viewport(1920, 1080); //define that we want the test to execute in full hd resolution
+
+      cy.visit(baseUrl); // Only works with prod
+      cy.wait(timeoutValue);
+
+      //Click on the sport that we want to bet
+      cy.get('div ul li span').contains(sportName).click();
+      cy.wait(timeoutValue);
+
+      
+      //Click on the team that we want to bet
+      cy.get(`span[aria-label="${fullTeamName}"]`).first().click();
+      cy.wait(timeoutValue);
+
+
+      //Click on the team that we want selected Puck and Money Line
+      betOnFirstTeam("Puck Line, Los Angeles");
+      cy.wait(timeoutValue);
+
+      betOnFirstTeam("Moneyline, Los Angeles");
+      cy.wait(timeoutValue);
+   
+      //Click again on Puck and Money Line of the team to unselect
+      betOnFirstTeam("Puck Line, Los Angeles");
+      cy.wait(timeoutValue);
+
+      betOnFirstTeam("Moneyline, Los Angeles");
+      cy.wait(timeoutValue);
+
+
+      //Click again to bet again in the team
+      betOnFirstTeam(fullTeamName);
+      cy.get('div').find('input[type="text"]').first().type('0.10');
+      cy.wait(timeoutValue);
+      })
+
+})
+
+function betOnFirstTeam(teamName) {
+  cy.get(`div[role="button"][aria-label*="${teamName}"]`).first().click();
+}
