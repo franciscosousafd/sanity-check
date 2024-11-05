4e3ad37de3b3db789d3a06c74854ee90585aab4e
franciscosousafd <francisco.sousa@fanduel.com>
2024-11-05 10:38:07 +0000

    chore: Update code to have string interpolation

--- cypress/e2e/0-Sanity/fanduel.invalidImply.cy.js
+++ cypress/e2e/0-Sanity/fanduel.invalidImply.cy.js
@@ -12,7 +12,7 @@ context('Actions', () => {
       const baseUrl = "https://co.sportsbook.fanduel.com/";
       const sportName = "NHL";
       const fullTeamName = "Los Angeles Kings";
-      
+
       cy.viewport(1920, 1080); //define that we want the test to execute in full hd resolution
 
       cy.visit(baseUrl); // Only works with prod
@@ -29,17 +29,17 @@ context('Actions', () => {
 
 
       //Click on the team that we want selected Puck and Money Line
-      betOnFirstTeam("Puck Line, Los Angeles");
+      betOnFirstTeam(`Puck Line, ${fullTeamName}`);
       cy.wait(timeoutValue);
 
-      betOnFirstTeam("Moneyline, Los Angeles");
+      betOnFirstTeam(`Moneyline, ${fullTeamName}`);
       cy.wait(timeoutValue);
    
       //Click again on Puck and Money Line of the team to unselect
-      betOnFirstTeam("Puck Line, Los Angeles");
+      betOnFirstTeam(`Puck Line, ${fullTeamName}`);
       cy.wait(timeoutValue);
 
-      betOnFirstTeam("Moneyline, Los Angeles");
+      betOnFirstTeam(`Moneyline, ${fullTeamName}`);
       cy.wait(timeoutValue);
 
 
